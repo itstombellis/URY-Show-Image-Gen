0549bd30deaf71440d58c0f0cc7e783eb775c576
Tom Bellis <thomas.bellis@btinternet.com>
2016-11-19 23:08:06 +0000

    Finalised Log Implementation

--- ShowImageCreator.py
+++ ShowImageCreator.py
@@ -26,8 +26,9 @@ def getShows():
             shows[data["payload"][show]["show_id"]] = data["payload"][show]["title"]
 
         return shows
-    except Exception as e:
-        log("Error","Could not acess API.",str(e))
+    except:
+        log("API","Could not acess API.")
+        sys.exit(0)
 
 
 def applyBrand(showName, outputName, branding):
@@ -100,7 +101,6 @@ def applyBrand(showName, outputName, branding):
 def normalize(input):
     words = input.split(" ")
     maxFirstLineLength = 13
-    maxOtherLinesLength = 22
     firstLine = ''
     otherLinesList = []
     firstLineFull = False
@@ -108,22 +108,23 @@ def normalize(input):
     for word in words:
         if firstLineFull == False:
             if (len(word) > maxFirstLineLength) and (len(firstLine) < maxFirstLineLength):
-                log("DCM", word +" is too long for image.")
+                log("DCM", word +" is too long for image.", showID)
                 break
             elif len(firstLine + word) <= maxFirstLineLength:
                 firstLine += str(word.upper()) + ' '
             else:
                 firstLineFull = True
-                otherLinesList = dealWithOtherLines(otherLinesList, word, maxOtherLinesLength)
+                otherLinesList = dealWithOtherLines(otherLinesList, word)
         else:
-            otherLinesList = dealWithOtherLines(otherLinesList, word, maxOtherLinesLength)
+            otherLinesList = dealWithOtherLines(otherLinesList, word)
     otherLines = "".join(item + "\n" for item in otherLinesList)
     return firstLine, otherLines
 
 
-def dealWithOtherLines(otherLinesList, word, maxOtherLinesLength):
+def dealWithOtherLines(otherLinesList, word):
+    maxOtherLinesLength = 22
     if len(word) > maxOtherLinesLength:
-        log("DCM","Word too long for image.","Within function dealWithOtherLines().")
+        log("DCM", "Word too long for image.", showID, "Within function dealWithOtherLines().")
         raise Exception
     elif len(otherLinesList) > 0 and (len(otherLinesList[-1]) + len(word) < maxOtherLinesLength):
         otherLinesList[-1] += " " + word
@@ -132,10 +133,10 @@ def dealWithOtherLines(otherLinesList, word, maxOtherLinesLength):
     return otherLinesList
 
 
-def log(type, message, errorMessage="No exception error message."):
+def log(type, message, showNum="", errorMessage="No exception error message."):
     f=open("logfile.log","a")
     curTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    f.write(curTime + " - [" + type.upper() + "] Show ID: {" + showID + "} " + message + "\n" + errorMessage + "\n")
+    f.write(curTime + " - [" + type.upper() + "] Show ID: {" + showNum + "} " + message + "\n" + errorMessage + "\n")
     f.close()
     if type=="DCM":
         pass #Call send email function to DCM
