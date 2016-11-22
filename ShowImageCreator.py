a2b1d57f7525347f6e42d68c84de5aaea2d1eff2
joshuaferr1s <jajf500@york.ac.uk>
2016-11-22 21:00:05 +0000

    Debug logs

--- ShowImageCreator.py
+++ ShowImageCreator.py
@@ -20,6 +20,7 @@ def getShows():
         The dictionary of shows with show ids mapping to the show title.
     """
     try:
+        log("DEBUG", "Running getShows() function.")
         data = requests.get(url).json()
         shows = {}
 
@@ -28,7 +29,7 @@ def getShows():
 
         return shows
     except:
-        log("API","Could not acess API.", debugMode)
+        log("API","Could not acess API.")
         sys.exit(0)
 
 
@@ -42,20 +43,27 @@ def applyBrand(showName, outputName, branding):
         The function outputs a JPG image to a sub folder called ShowImages.
     """
     # Hack to get branding from show name
+    log("DEBUG", "Running applyBrand() function.", showID)
     branding = brandingFromShowName(showName)
     showName = stripPrefix(showName)
     # Determines which overlay to apply to the show image.
     if branding == "Speech":
+        log("DEBUG", "Show branding should be speech.", showID)
         brandingOverlay = "GreenSpeech.png"
     elif branding == "News":
+        log("DEBUG", "Show branding should be news.", showID)
         brandingOverlay = "BlueGeneral.png"
     elif branding == "Music":
+        log("DEBUG", "Show branding should be music.", showID)
         brandingOverlay = "PurpleMusic.png"
     elif branding == "OB":
+        log("DEBUG", "Show branding should be OB.", showID)
         brandingOverlay = "RedOB.png"
     elif branding == "Old":
+        log("DEBUG", "Show branding should be old.", showID)
         brandingOverlay = "WhitePreShowImageFormat.png"
     else:
+        log("DEBUG", "Show branding should be generic show.", showID)
         brandingOverlay = "BlueGeneral.png"
 
 # Determines which background image to use for the show image.
@@ -66,6 +74,7 @@ def applyBrand(showName, outputName, branding):
     img.paste(overlay, (0, 0), overlay)
 
 # First line formatting
+    log("DEBUG", "Formatting the first line.", showID)
     firstLineText, otherLines = normalize(showName)
     firstLineFontSize = 85
     # firstLineFont = ImageFont.truetype(<font-file>, <font-size>)
@@ -79,6 +88,7 @@ def applyBrand(showName, outputName, branding):
     draw.text(((800-w)/2, firstLineHeight),firstLineText,(255,255,255),firstLineFont, align='center')
 
 # Other Lines Show Text formatting
+    log("DEBUG", "Formatting further lines.", showID)
     otherLinesTextSize = 50
     # firstLineFont = ImageFont.truetype(<font-file>, <font-size>)
     otherLinesFont = ImageFont.truetype("Raleway-LightItalic.ttf", otherLinesTextSize)
@@ -91,6 +101,7 @@ def applyBrand(showName, outputName, branding):
     draw.text(((800-w)/2, otherLinesTextHeight), otherLines,(255,255,255),otherLinesFont, align='center')
 
 # website URY formatting
+    log("DBEUG", "Applying website branding.", showID)
     websiteURL = 'URY.ORG.UK/LIVE \n @URY1350'
     websiteFont = ImageFont.truetype("Raleway-SemiBoldItalic.ttf", otherLinesTextSize)
     draw = ImageDraw.Draw(img)
@@ -100,46 +111,62 @@ def applyBrand(showName, outputName, branding):
     # draw.text((x, y),"Sample Text",(r,g,b))
     draw.text(((800-w)/2, websiteURLHeight), websiteURL,(255,255,255),otherLinesFont, align='center')
 
-# Saves the image as the output name in a subfolder ShowImages    
+# Saves the image as the output name in a subfolder ShowImages
+    log("DEBUG", "Saving the final image.", showID)
     img.save('ShowImages/%s.jpg' %outputName)
 
 def brandingFromShowName(showName):
     if showName[:13] == "URY Presents:":
+        log("DEBUG", "Applying OB branding.", showID)
         output = 'OB'
     elif showName == "The URY Pantomime 2016: Beauty and the Beast":
+        log("DEBUG", "Applying OB branding.", showID)
         output = 'OB'
     elif showName[:1] == "#":
+        log("DEBUG", "Applying OB branding.", showID)
         output = 'OB'
     elif showName == "Georgie and Angie's Book Corner":
+        log("DEBUG", "Applying speech branding.", showID)
         output = 'Speech'
     elif showName == "Stage":
+        log("DEBUG", "Applying speech branding.", showID)
         output = 'Speech'
     elif showName == "Speech Showcase":
+        log("DEBUG", "Applying speech branding.", showID)
         output = 'Speech'
     elif showName == "Screen":
+        log("DEBUG", "Applying speech branding.", showID)
         output = 'Speech'
     elif showName == "URY Newshour":
+        log("DEBUG", "Applying news branding.", showID)
         output = 'News'
     elif showName == "York Sport Report":
+        log("DEBUG", "Applying news branding.", showID)
         output = 'News'
     elif showName == "URY:PM - (( URY Music ))":
+        log("DEBUG", "Applying music branding.", showID)
         output = 'Music'
     else:
+        log("DEBUG", "No branding to be applied.", showID)
         output = ''
     return output
 
 
 def stripPrefix(showName):
     if showName[:12] == "URY Brunch -":
+        log("DEBUG", "Removing 'URY Brunch -' from the title.", showID)
         output = showName[12:]
     elif showName[:8] == "URY:PM -":
+        log("DEBUG", "Removing 'URY:PM -' from the title.", showID)
         output = showName[8:]
     else:
+        log("DEBUG", "No prefix to be removed from the title.", showID)
         output = showName
     return output
 
 
 def normalize(input):
+    log("DEBUG", "Running normalize() function.", showID)
     words = input.split(" ")
     maxFirstLineLength = 13
     firstLine = ''
@@ -152,7 +179,7 @@ def normalize(input):
                 firstLineFull = True
                 otherLinesList = dealWithOtherLines(otherLinesList, word)
             elif (len(word) > maxFirstLineLength) and (len(firstLine) < maxFirstLineLength):
-                log("DCM", word +" is too long for first line of image.", showID, debugMode)
+                log("DCM", word +" is too long for first line of image.", showID)
                 break
             elif len(firstLine + word) <= maxFirstLineLength:
                 firstLine += str(word) + ' '
@@ -166,9 +193,10 @@ def normalize(input):
 
 
 def dealWithOtherLines(otherLinesList, word):
+    log("DEBUG", "Running otherLinesList() function.", showID)
     maxOtherLinesLength = 22
     if len(word) > maxOtherLinesLength:
-        log("DCM", "Word too long for image.", showID, "Within function dealWithOtherLines().", debugMode)
+        log("DCM", "Word too long for image.", showID, "Within function dealWithOtherLines().")
         raise Exception
     elif len(otherLinesList) > 0 and (len(otherLinesList[-1]) + len(word) < maxOtherLinesLength):
         otherLinesList[-1] += " " + word
@@ -177,7 +205,7 @@ def dealWithOtherLines(otherLinesList, word):
     return otherLinesList
 
 
-def log(typeM="DEBUG", message="NONE", showNum="NULL", errorMessage="No exception error message.", debugMode=False):
+def log(typeM="DEBUG", message="NONE", showNum="NULL", errorMessage="No exception error message."):
     if  debugMode == True || typeM == "DCM" || typeM == "API":
         f=open("logfile.log","a")
         curTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
@@ -193,7 +221,7 @@ def log(typeM="DEBUG", message="NONE", showNum="NULL", errorMessage="No exceptio
 #### Uses API To Get Shows #####
 ################################
 ################################
-
+log("DEBUG", "Program Started!")
 ShowsDict = getShows()
 
 print(debugMode)
@@ -205,3 +233,4 @@ for key in ShowsDict:
     branding = 'OB'
 
     applyBrand(showName, showID, branding)
+log("DEBUG", "Program Complete!")
