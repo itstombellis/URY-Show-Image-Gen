34c6ed066a3f99fb7580bef252bfcc6361d52d9d
Tom Bellis <thomas.bellis@btinternet.com>
2018-04-04 11:45:33 +0100

    Actually use the new backgrounds

--- ShowImageCreator.py
+++ ShowImageCreator.py
@@ -9,7 +9,7 @@ backgroundImagePath = "GenericShowBackgrounds/"
 colouredBarsPath = "ColouredBars/"
 try:
 	apiKey = sys.argv[1]
-	url = "https://ury.org.uk/api/v2/show/allshows?current_term_only=1&api_key=" + apiKey
+	url = "https://ury.org.uk/api/v2/show/allshows?current_term_only=0&api_key=" + apiKey
 	debugMode = sys.argv[2]
 except IndexError as e:
 	log("ERROR", "System Argument(s) not passed in.", str(e))
@@ -27,7 +27,10 @@ def getShows():
     	shows = {}
 
     	for show in data["payload"]:
-    		shows[data["payload"][show]["show_id"]] = data["payload"][show]["title"]
+            showID = data["payload"][show]["show_id"]
+            showTitle = data["payload"][show]["title"]
+            if showID != 13031:
+                shows[showID] = showTitle
 
     	return shows
     except IOError as e:
@@ -49,6 +52,7 @@ def applyBrand(showName, outputName, branding):
     ##########################################
     ##########################################
     ## Hack to get branding from show name
+    #branding = "Old"
     branding = brandingFromShowName(showName)
     ##########################################
     ##########################################
@@ -88,7 +92,7 @@ def applyBrand(showName, outputName, branding):
             
 # Determines which background image to use for the show image.
     try:
-        img = Image.open(backgroundImagePath + str(randint(1,16)) +".png")
+        img = Image.open(backgroundImagePath + str(randint(1,25)) +".png")
     except IOError as e:
         log("Error", "Background image could not be opened.", str(e))
 
@@ -301,7 +305,8 @@ def log(typeM="DEBUG", message="NONE", showNum="NULL", errorMessage="No exceptio
 ################################
 ################################
 log("DEBUG", "Program Started!")
-ShowsDict = getShows()
+#ShowsDict = getShows()
+ShowsDict = {12209: 'URY:PM - URY Chart Show', 12868: 'URY:PM - Roku Radio', 12374: 'URY Newshour', 12928: 'No Ducks Given', 12957: '(20,000 Leagues) Into the Void', 13000: "What's That Topic?", 13008: 'Stage', 13049: 'York Sport Report', 13053: 'National Award Nominated URY:PM with National Award Nominated K-Spence', 13065: 'URY Brunch: The Saturday Lie-In', 13067: "Georgie and Angie's Book Corner", 13070: 'Gully Riddems', 13071: 'Indie Unearthed', 13073: 'Building Bridges - The Road to Rock and Roll', 13074: 'Castle Sessions', 13120: 'Pardon my French', 13123: 'Your Opinion is Wrong', 13125: 'The Night Call', 13126: 'InsomniHour', 13130: "What's on my playlist?", 13134: "Diggin' Deep", 13140: 'Barry Tomes', 13141: 'Topics & Tunes', 13149: 'RapChat', 13156: 'Morning Glory', 13159: 'The 20th Century Collection', 11628: 'Retrospectre!', 13167: 'Star Struck Jack and The Mystery Cat', 13177: 'Kick back Sundays with Kate ', 13179: 'URY:PM (( URY Music ))', 13184: 'The Late Night Bass Podcast', 13186: 'These Charming Girls', 13189: 'Grumpy Youngish Men', 13192: "Leckie's listeners", 13202: 'Dylan with a Mike!', 13204: 'The Right Faces For Radio', 13209: 'Things Can Only Get Bitter', 13227: 'Fringe: Full Metal Racket', 13228: 'URY Brunch: Star-Struck Jack and the Mystery Cat', 13232: 'URY Brunch - Breakfast Club', 13233: "URY Brunch - We're All Ears", 13235: 'Catchy Chunes', 13245: 'In Between Days', 13246: 'No DLC Required', 13255: 'The Breakz Showcase ', 13256: 'URY Brunch - Amateur Hour', 13257: "Grandad's Jazz", 13258: 'URY:PM - Peculiarities', 13259: 'Tales from the Phantasmagoria', 13260: 'URY:PM - Willis Weekly', 13261: 'Almost Audible', 13262: 'Cream Cheese', 13263: "#URYonTOUR: Freshers' 2016", 13264: 'URY:PM - No Ducks Given', 13265: 'Screen', 13266: 'URY Brunch - The Culture Show', 13267: 'URY Whisper Show', 13268: 'URY Brunch - Smile!', 13269: 'Midweek Marauders', 13270: 'Hidden Gems', 13271: 'Go Funk Yourself', 13272: 'The Brighter Side of Life', 13273: "Peck's Picks", 13274: 'Toons in the Afternoons', 13275: 'Roger That', 13276: 'The Eclectic Mix', 13277: 'The Alternative Music Show', 13278: 'DESERT ISLAND DISCO', 13279: 'coHEARence', 13280: 'Formula 1 Analysis', 13282: 'AM-bassador', 13283: 'Liv and the guy', 13284: 'Monday Chills ', 13285: 'Your Weekend', 13287: 'Non-Stop-Tom', 13288: 'Cool Britannia', 13289: 'Nothing but Chuuuunes with Hayds', 13290: 'The Ben and Jasper Show', 13291: 'Why Not?', 13292: 'Alternative Juice', 13293: "Chef Will - Where there's a Will, there's a...", 13294: 'NOUVEAU.', 13295: 'Kiltie Pleasures with Jonny ', 13296: 'Brain Waves', 13297: 'The Sounds of Time', 13298: "Josh and Tom's Afternoon Antics", 13299: 'Music for Old People', 13300: 'GAYdio', 13301: 'PolChat', 13302: 'Vanbrugh Chair Debate', 13303: 'URY Presents: UYCB 2016 Winter Concert', 13304: 'NICturnal', 13305: 'Speech Showcase', 13306: 'More Songs About Chocolate And Girls...', 13307: 'URY Does RAG Courtyard Takeover'}
 
 for key in ShowsDict:
     showName = ShowsDict[key]
